e272774e633de2edc5754f1139ebd3722432b487
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-26 14:19:21 +0000

    more cats

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,16 +10,20 @@ def home():
 
 @app.route("/categories")
 def categories():
-    categories = Categories.query.order_by.all(Category.category_name)
+    categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        category = list(
-            Category(category_name=request.form.get("category_name")))
+        category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category", methods=["GET", "POST"])
+def edit_category():
+    render_template("edit_category.html")
